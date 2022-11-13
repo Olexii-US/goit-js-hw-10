bd60b0179e7040dc9ea2af71980e26d6453d9235
Olexii-US <alyosha.us@gmail.com>
2022-11-14 00:05:05 +0200

    css finished

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio'
 import { fetchCountries } from "./fetchCountries"
 
 const DEBOUNCE_DELAY = 300;
+const countryMaxforPage = 10;
 
 const refs = {
     input: document.querySelector("#search-box"),
     list: document.querySelector(".country-list"),
-    info: document.querySelector(".country-info")
+    info: document.querySelector(".country-info")    
 }
 
 refs.input.addEventListener("input", debounce((input) => {
     const countries = input.target.value.trim()
 
     if (countries === "") {
-        refs.list.innerHTML = "";
-        return
+        return clearNarkup()
+        
     }
     
     fetchCountries(countries).then(name => markupCountries(name))
@@ -25,45 +26,49 @@ refs.input.addEventListener("input", debounce((input) => {
 }, DEBOUNCE_DELAY)) 
 
 function markupCountries(name) {
-
-    if (name.length > 10) {
+    if (name.length > countryMaxforPage) {
         return Notify.info("Too many matches found. Please enter a more specific name.")
-    } else if (name.length > 1 && name.length <= 10) {
+    } else if (name.length === 1) {
         markupCountryList(name);
-        console.log("eeeeeeeeeelllllllsssssseeeee")
-    } else
-    markupSingleCountry(name);
+        markupCountryData(name);
 
-}
-
-function markupSingleCountry(name) {
-    const markup = name.map((el) => {
-        const languageSet = Object.values(el.languages)
-        
-        return `<li>
-            <h2>${el.name.official}</h2>
-            <img src="${el.flags.svg}" alt="Country flag" width="100">
-                <p>Capital: ${el.capital}</p>
-                <p>Population: ${el.population}</p>
-                <p>Languages: ${languageSet}</p>
-        </li>`
-    })
-        .join("");
-    refs.list.innerHTML = markup;
+        ///////// for CSS//////
+        const liRef = document.querySelectorAll(".country-item")
+        console.log(liRef)
+        liRef[0].classList.replace('country-item', 'item')
+        ///////
+    } else if (name.length > 1 && name.length <= countryMaxforPage) {
+        clearNarkup()
+        markupCountryList(name);
+    }
 }
 
 function markupCountryList(name) {
     const markup = name.map((el) => {
-        return `<li>
-            <h2>${el.name.official}</h2>
-            <img src="${el.flags.svg}" alt="Country flag" width="100">
+        return `<li class="country-item">
+            <img src="${el.flags.svg}" alt="Country flag">
+            <h2>${el.name.official}</h2>            
         </li>`
     })
         .join("");
     refs.list.innerHTML = markup;
 }
 
+function markupCountryData(name) {
+    const markup = name.map((el) => {
+        const languageSet = Object.values(el.languages)
+        
+        return `<p><b>Capital:</b> ${el.capital}</p>
+                <p><b>Population:</b> ${el.population}</p>
+                <p><b>Languages:</b> ${languageSet.join(", ")}</p>`
+    })
+        .join("");
+    refs.info.innerHTML = markup;
+}
 
-
+function clearNarkup() {
+    refs.list.innerHTML = "";
+    refs.info.innerHTML = "";
+}
 
 
